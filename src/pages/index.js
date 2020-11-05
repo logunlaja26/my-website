a5f64dcc42f12037c8b20390f49f88be2530f0ff
logunlaja26 <logunlaja26@gmail.com>
2020-11-05 10:35:41 -0600

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+    <div>
+      <h1>Hello.</h1>
+      <h2>Im Ly,  a fullstack developer living in Chicago.</h2>
+    </div>
+  )
 }
