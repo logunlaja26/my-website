63df85e3347cbb3bf09a9ae90f16bbdfb59d5765
logunlaja26 <logunlaja26@gmail.com>
2020-11-29 11:03:05 -0600

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
           trackingId: 'G-X0QLFMRYDM',
-          // this option places the tracking script into the head of the DOM
+          // this option places the tracking script into the head of the DOM.
           head: true,
           anonymize: true,
         },
