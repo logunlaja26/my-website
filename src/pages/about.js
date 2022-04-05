ea7e6b9856e2a12be697c4a6da9830fb3fecd26f
logunlaja26 <logunlaja26@gmail.com>
2022-04-05 17:57:13 -0500

    Revert "revert Added gatsby-plugin-robots-txt plugin to ensure google only crawls for"
    This reverts commit 0d494691d4e8873cc9d8ab2ab9db5291ab8e1d85.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
-import { Link } from "gatsby"
 
 export default function AboutPage() {
   return (
@@ -9,12 +8,6 @@ export default function AboutPage() {
       <Head title="About" />
       <h1>Ly Ogunlaja</h1>
       <p>About Page</p>
-      <p>
-        Software development has been a means for me to exercise creative
-        thinking to solve problems. I'm a professional software engineer that
-        likes to hack Software Development <Link to="/projects">Tools </Link>to
-        make lives easier.{" "}
-      </p>
       <p>
         follow me on Twitter
         <a href="https://twitter.com/CoderBlack92" target="_blank">
